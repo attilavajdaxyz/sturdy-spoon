33c3d1046b151baac12ce741fc7e871a9f1e48c1
attilvajdaxyz <46789083+viyek@users.noreply.github.com>
2022-04-21 20:58:43 +0200

    added daily blog entry and css element 😉🐈‍⬛

---
+++ calc.rb
@@ -0,0 +1,37 @@
+# how many hours are in a year?
+
+year = 1
+days_in_a_year = 365
+hours_in_a_day = 24
+
+hours_in_a_year = days_in_a_year * hours_in_a_day
+
+puts hours_in_a_year
+
+# how many minutes are in a decade?
+
+minutes_in_a_year = 60 * hours_in_a_year
+
+minutes_in_a_decade = 10 * minutes_in_a_year
+
+puts minutes_in_a_decade
+
+# how many seconds old are you?
+
+seconds_in_a_year = 60 * minutes_in_a_year
+
+my_age = 34
+
+seconds_old_I_am = 34 * seconds_in_a_year
+
+puts seconds_old_I_am
+
+# how many chocolates do you hope to eat in your life?
+
+# 🎹
+
+# If I am 1406 million seconds old, how old am I? 
+
+your_age_in_seconds = 1406_000_000 * seconds_in_a_year
+
+puts your_age_in_seconds
