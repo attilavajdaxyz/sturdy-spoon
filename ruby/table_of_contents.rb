c0c634f7f825cfd5617a52ba0c9e9f8334d2892d
attilvajdaxyz <46789083+viyek@users.noreply.github.com>
2022-04-27 09:31:09 +0200

    added daily blog post

---
+++ ruby/table_of_contents.rb
@@ -0,0 +1,19 @@
+#Table of Contents                
+                                                 
+# Chapter 1:  Numbers                        page 1
+# Chapter 2:  Letters                       page 72
+# Chapter 3:  Variables                    page 118
+
+
+# lineWidth = 50
+# puts "Table of Contents".center
+# puts "Chapter 1:  Numbers".ljust(lineWidth/2) + "page1".rjust(lineWidth/2)
+# puts "Chapter 2:  Letters".ljust(lineWidth/2) + "page72".rjust(lineWidth/2)
+# puts "Chapter 3:  Variables".ljust(lineWidth/2) + "page118".rjust(lineWidth/2)
+
+lineWidth = 50
+header = "Table of Contents"
+puts header.center lineWidth
+puts "Chapter 1:  Numbers".ljust(lineWidth/2) + "page1".rjust(lineWidth/2)
+puts "Chapter 2:  Letters".ljust(lineWidth/2) + "page72".rjust(lineWidth/2)
+puts "Chapter 3:  Variables".ljust(lineWidth/2) + "page118".rjust(lineWidth/2)
---
+++ unix/helloworld.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+echo 'Hello World!'
+
+PRICE_PER_APPLE=5
+MyFirstLetters=ABC
+greeting="hello      world!"
+
+echo "The price of an Apple today is: \$HK $PRICE_PER_APPLE"
+
+echo "The first 10 letters in the alphabet are: ${MyFirstLetters}DEFGHIJ"
+
+echo $greeting" now with spaces: $greeting"
