1c6a5067153bccd6b05cef4bc8049c0700559560
attilvajdaxyz <46789083+viyek@users.noreply.github.com>
2022-05-11 10:06:18 +0200

    added daily blog post

---
+++ javascript/falling_sand.js
@@ -0,0 +1,25 @@
+class Grid {
+    initialize(width, height) {
+        this.width = width;
+        this.height = height;
+        this.grid = new Array(width * height).fill(0);
+    }
+}
+
+class Grid {
+    initialize(width, height) {
+        this.width = width;
+        this.height = height;
+        this.grid = new Array(width * height).fill(0)
+    }
+}
+
+this.grid = new Array(width * height)fill.(0)
+
+this.grid = new Array(width * height).fill(0)
+
+class Grid {
+  this.width = width;
+  this.height = height;
+  this.grid = new Array(width * height).fill(0)
+}
