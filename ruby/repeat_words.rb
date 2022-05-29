9db8749df2888f28b2642901b2d27882306765de
attilvajdaxyz <46789083+viyek@users.noreply.github.com>
2022-05-29 14:26:32 +0200

    added daily blog post

---
+++ ruby/repeat_words.rb
@@ -0,0 +1,3 @@
+puts "Please write a number of words and press ENTER"
+array = gets.chomp.split(' ')
+puts array.sort
--- ruby/table_of_contents.rb
+++ ruby/table_of_contents.rb
@@ -11,9 +11,9 @@
 # puts "Chapter 2:  Letters".ljust(lineWidth/2) + "page72".rjust(lineWidth/2)
 # puts "Chapter 3:  Variables".ljust(lineWidth/2) + "page118".rjust(lineWidth/2)
 
+table_of_contents = ["Table of Contents", "Chapter 1:  Numbers", "Chapter 2:  Letters", "Chapter 3:  Variables"]
 lineWidth = 50
-header = "Table of Contents"
-puts header.center lineWidth
-puts "Chapter 1:  Numbers".ljust(lineWidth/2) + "page1".rjust(lineWidth/2)
-puts "Chapter 2:  Letters".ljust(lineWidth/2) + "page72".rjust(lineWidth/2)
-puts "Chapter 3:  Variables".ljust(lineWidth/2) + "page118".rjust(lineWidth/2)
+puts table_of_contents[0].center lineWidth
+puts table_of_contents[1].ljust(lineWidth/2) + "page1".rjust(lineWidth/2)
+puts table_of_contents[2].ljust(lineWidth/2) + "page72".rjust(lineWidth/2)
+puts table_of_contents[3].ljust(lineWidth/2) + "page118".rjust(lineWidth/2)
